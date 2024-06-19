1dd9da167f4e6c44a315a83155ef42e3a0205693
darthjee <darthjee@gmail.com>
2024-06-19 15:14:26 -0300

    Add seed

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+Zyra
+  .register(User, find_by: :email)
+  .on(:build) { |user| user.password = SecureRandom.hex(10) }
+
+Zyra.find_or_create(
+  :user,
+  email: 'email@srv.com',
+  login: 'user',
+  name: 'user',
+  password: '123456'
+)
