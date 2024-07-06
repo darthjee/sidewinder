50046bca2fd8a84a5e54043291f7044dff06ed2d
darthjee <darthjee@gmail.com>
2024-07-06 18:46:36 -0300

    Add ajax test

--- source/spec/controllers/concerns/admin_page_spec.rb
+++ source/spec/controllers/concerns/admin_page_spec.rb
@@ -11,37 +11,79 @@ describe AdminPage do
   end
   # rubocop:enable RSpec/DescribedClass
 
-  context 'when user is not logged' do
-    before do
-      get :index, params: {}
+  context 'when requesting HTML without ajax' do
+    let(:parameters) { {} }
+
+    context 'when user is not logged' do
+      before do
+        get :index, params: parameters
+      end
+
+      it 'performs redirection to login page' do
+        expect(response).to redirect_to('/#/login')
+      end
     end
 
-    it 'performs redirection to login page' do
-      expect(response).to redirect_to('/#/login')
+    context 'when user is logged but not as admin' do
+      include_context 'with logged user', admin: false
+
+      before do
+        get :index, params: parameters
+      end
+
+      it 'performs redirection to forbidden page' do
+        expect(response).to redirect_to('/#/forbidden')
+      end
+    end
+
+    context 'when user is logged as admin' do
+      include_context 'with logged user', admin: true
+
+      before do
+        get :index, params: parameters
+      end
+
+      it 'Performs regular angular redirection' do
+        expect(response).to redirect_to('#/anonymous')
+      end
     end
   end
 
-  context 'when user is logged but not as admin' do
-    include_context 'with logged user', admin: false
+  context 'when requesting HTML with ajax'do
+    let(:parameters) { { ajax: true } }
 
-    before do
-      get :index, params: {}
+    context 'when user is not logged' do
+      before do
+        get :index, params: parameters
+      end
+
+      it 'performs redirection to login page' do
+        expect(response).to redirect_to('/#/login')
+      end
     end
 
-    it 'performs redirection to forbidden page' do
-      expect(response).to redirect_to('/#/forbidden')
+    context 'when user is logged but not as admin' do
+      include_context 'with logged user', admin: false
+
+      before do
+        get :index, params: parameters
+      end
+
+      it 'performs redirection to forbidden page' do
+        expect(response).to redirect_to('/#/forbidden')
+      end
     end
-  end
 
-  context 'when user is logged as admin' do
-    include_context 'with logged user', admin: true
+    context 'when user is logged as admin' do
+      include_context 'with logged user', admin: true
 
-    before do
-      get :index, params: {}
-    end
+      before do
+        get :index, params: parameters
+      end
 
-    it 'Performs regular angular redirection' do
-      expect(response).to redirect_to('#/anonymous')
+      it 'Performs regular angular redirection' do
+        expect(response).to redirect_to('#/anonymous')
+      end
     end
   end
 end
